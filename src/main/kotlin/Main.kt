015ac8a0ba0e5a6a9c1fe4d835553ea4b45f05ff
juseong <wjdwntjd55@naver.com>
2023-02-28 15:27:06 +0900

    실습 1. 인사말과 사용자에게 입력 받은 정보 출력

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // Step 1. 인사말 출력
+    println("안녕하세요. Shoppi 에 오신 것을 환영합니다!")
+    println("쇼핑을 계속 하시려면 이름을 입력해주세요 :)")
+
+    // Step 2. 사용자 이름받기
+    val name = readln()
+
+    // Step 3. 입력 받은 사용자 이름 출력
+    println("""
+        감사합니다. 반갑습니다, $name 님
+        원하시는 카테고리를 입력해주세요.
+        ***============================***
+        """.trimIndent())
+
 }
